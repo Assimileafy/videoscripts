f78e0603546e13acd46601c18aa9863174ce349a
assimileafy <nemi@leafeon.dev>
2024-06-08 19:24:27 -0400

    final touches, made silent, passes back filepath

--- grabaudio.sh
+++ grabaudio.sh
@@ -7,16 +7,14 @@ validate_piv(){
 if [ -z "$1" ]; then
 	echo "You need to pass in a valid Youtube URL ya dingus"
 	return 1 #stop
-else
-	echo "you passed in $URL"
-	#continue
 fi
-
-if [ -z "$2" ]; then
-	echo "Setting output name as default output.mp3" #fix the users mistake
-	FINAL_FILE="output.mp3"
-	echo "I have fixed your mistake, pray that I do not fix you further"
-	return 0
+	if [ -z "$2" ]; then
+		#echo "Setting output name as default output.mp3" #fix the users mistake
+		FINAL_FILE="output.mp3"
+		#echo "I have fixed your mistake, pray that I do not fix you further"
+		rm ./output.mp3
+		return 0
+	
 else
 	echo "output filename will be $2"
 	return 0
@@ -30,11 +28,12 @@ if [[ $1 =~ ^https?://(www\.)?youtube\.com/watch\?v=[a-zA-Z0-9_-]+$ ]]; then
 	if curl --output /dev/null --silent --head --fail "$1"; then
 		return 0
 	else
-		echo "Youtube is either down or the video is gone"
+		#echo "Youtube is either down or the video is gone"
+		echo "error_youtube_down"
 		return 1
 	fi
 else
-	echo "Invalid Youtube URL format."
+	echo "error_youtube_url_invalid"
 	return 1
 fi	
 	}
@@ -46,27 +45,29 @@ FINAL_FILE="$2"
 
 # check if the passed in values are correct
 if ! validate_piv "$URL" "$FINAL_FILE"; then
-	echo "exiting, user didn't give a url or filename"
+	#echo "exiting, user didn't give a url or filename"
+	echo "error_bad_url_or_filename"
 	exit 1
 fi
 
 # validate the url
 if ! validate_url  "$URL"; then
-	echo "this url empty or broken! YEET!"
+	#echo "this url empty or broken! YEET!"
+	echo "error_url_broken"
 	exit 1
 fi
 
 
 # download the stupid video
-youtube-dl -x --audio-format m4a --output "$TEMP_FILE" "$URL"
+youtube-dl --no-call-home -q -x --audio-format m4a --output "$TEMP_FILE" "$URL"
 
-echo "The file has been saved at $TEMP_FILE"
-echo "We will now try to convert this, I hope you passed in a good filename"
+#echo "file has been saved as $TEMP_FILE"
+#echo "converting filename"
 
 # convert the gremlin noises into kinda noises
-ffmpeg -i "$TEMP_FILE" -vn -ab 128k "$FINAL_FILE" -loglevel 0
-
-# mop up the gremlin remains
+ffmpeg -i "$TEMP_FILE" -hide_banner -v 8 -n -ab 128k "$FINAL_FILE"
+#echo "cleaning up"
 rm "$TEMP_FILE"
-
-# echo "The gremlin has been captured and squeezed for the goodies, it's at $FINAL_FILE"
+#echo "$FINAL_FILE"
+FULL_PATH=$(realpath "$FINAL_FILE")
+echo "$FULL_PATH"
