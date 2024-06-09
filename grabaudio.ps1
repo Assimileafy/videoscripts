95ea12bb85dd872019c7ddbd9233f0d1683807fa
assimileafy <nemi@leafeon.dev>
2024-06-08 20:28:48 -0400

    Add wsl compatibility

---
+++ grabaudio.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$url,
+    [Parameter(Mandatory=$false, Position=1)]
+    [string]$filename
+)
+
+#take in input for $youtubeurl
+$youtubeurl = $url
+
+if ([string]::IsNullOrEmpty($filename)) {
+    $filename = "output.mp3"
+}
+
+if (Test-Path $filename) {
+    Remove-Item $filename -Force
+    Write-Output "The file '$filename' existed and has been deleted."
+}
+
+#pass input into wsl
+$output = wsl ./grabaudio.sh $youtubeurl $filename
+Write-Output $output
+
+# Convert it to a windows file path
+$WindowsPath = wsl wslpath -w "$output"
+
+# Output it back out for windows to use
+Write-Output $WindowsPath
